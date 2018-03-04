70ae9852e1cac703bacb36e64c24655a5c8e0e6d
andy.pranata <andy.pranata@dwp.co.id>
2018-03-04 17:17:10 +0700

    first commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+from os.path import join, dirname
+
+setup(name='ethminerlib',
+      version='0.0.1',
+      description='ethminerlib',
+      url='http://repo.dwp.io/sadnet/engineering-library.git',
+      author='dnet',
+      author_email='sa@dwp.co.id',
+      license='DNET License',
+      packages=find_packages(),
+      zip_safe=False)
